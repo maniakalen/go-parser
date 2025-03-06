90ac8dc91cd077575f28aa423ab84ae6f893a95c
maniakalen <pesho10@yahoo.com>
2025-03-06 09:31:38 +0200

    Initial commit

---
+++ generic.go
@@ -0,0 +1,61 @@
+package generic
+
+import (
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func ParseHtml(body string, keywords []string) (bool, error) {
+	bodyReader := strings.NewReader(string(body))
+	doc, err := html.Parse(bodyReader)
+	if err != nil {
+		slog.Error("unable to parse page body: " + err.Error())
+		return false, fmt.Errorf("unable to parse page body")
+	}
+	return scanNode(doc, keywords)
+}
+
+func scanNode(n *html.Node, keywords []string) (bool, error) {
+	if n.Type == html.ElementNode && (n.Data == "title") {
+		for _, keyword := range keywords {
+			if strings.Contains(n.FirstChild.Data, keyword) {
+				return true, nil
+			}
+		}
+	}
+	if n.Type == html.ElementNode && n.Data == "meta" {
+		check := false
+		for _, a := range n.Attr {
+			if a.Key == "name" && (a.Val == "description" || a.Val == "keywords") {
+				check = true
+			}
+			if a.Key == "content" && check {
+				for _, keyword := range keywords {
+					if strings.Contains(a.Val, keyword) {
+						return true, nil
+					}
+				}
+			}
+		}
+	}
+	if n.Type == html.ElementNode && n.Data == "h1" {
+		for _, keyword := range keywords {
+			if strings.Contains(n.FirstChild.Data, keyword) {
+				return true, nil
+			}
+		}
+	}
+
+	for child := n.FirstChild; child != nil; child = child.NextSibling {
+		if found, err := scanNode(child, keywords); found {
+			if err != nil {
+				return false, err
+			}
+			return true, nil
+		}
+	}
+	return false, nil
+}
---
+++ generic_test.go
@@ -0,0 +1,60 @@
+package generic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenericParser(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		body     string
+		keywords []string
+		result   bool
+	}{
+		{
+			name:     "Test title",
+			body:     "<html><title>test title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>test h1</h1></html>",
+			keywords: []string{"test", "title"},
+			result:   true,
+		},
+		{
+			name:     "Some mitle",
+			body:     "<html><title>some title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>some h1</h1></html>",
+			keywords: []string{"mitle"},
+			result:   false,
+		},
+		{
+			name:     "Some title",
+			body:     "<html><title>some title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>some h1</h1></html>",
+			keywords: []string{"test", "title"},
+			result:   true,
+		},
+		{
+			name:     "Some description",
+			body:     "<html><title>some title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>some h1</h1></html>",
+			keywords: []string{"description"},
+			result:   true,
+		},
+		{
+			name:     "Some keywords",
+			body:     "<html><title>some title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>some h1</h1></html>",
+			keywords: []string{"keywords"},
+			result:   true,
+		},
+		{
+			name:     "Some h1",
+			body:     "<html><title>some title</title><meta name=\"description\" content=\"some description\" /><meta name=\"keywords\" content=\"some keywords\" /><h1>some hashone</h1></html>",
+			keywords: []string{"hashone"},
+			result:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, _ := ParseHtml(tt.body, tt.keywords)
+			assert.Equal(t, tt.result, result, "Expected result to be %v, got %v", tt.result, result)
+		})
+	}
+}
